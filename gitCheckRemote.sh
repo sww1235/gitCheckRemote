150c47466b35cb1c970e27431e4e1b95e021f065
sww1235 <sww1235@gmail.com>
2016-06-10 00:58:48 -0600

    fixed verbose flag in git remote update

--- gitCheckRemote.sh
+++ gitCheckRemote.sh
@@ -9,17 +9,17 @@ let count_unchanged=0
 let verbose=0
 
 # Find git repos and loop over them:
-for repo in `find . -type d -name ".git"` # find returns a relative path with the .git directory at the 
+for repo in `find . -type d -name ".git"` # find returns a relative path with the .git directory at the
 do
     let count_all=${count_all}+1
-    
+
     # cd to the dir that contains .git/:
     dir=`echo ${repo} | sed -e 's/\/.git/\//'` #replaces the /.git on the end of the returned path with /
     cd ${dir}
-    
+
     # update remote refs
-    git remote update -v
-    
+    git remote -v update
+
     # If there are changes, print some status and branch info of this repo:
     git status --porcelain | grep -v '??' &> /dev/null && { # first has to succeed for second command to run
 	echo -e "\n\n \E[1;31m ${dir}\E[0m"
@@ -27,13 +27,13 @@ do
 	git status -s | grep -v '??'
 	let count_changed=${count_changed}+1
     }
-    
+
     # If verbose, print info in the case of no changes:
     git status -s | grep -v '??' &> /dev/null || {
 	if [ ${verbose} -ne 0 ]; then echo "Nothing to do for ${dir}"; fi
 	let count_unchanged=${count_unchanged}+1
     }
-    
+
     # cd back:
     cd - &> /dev/null
 done
