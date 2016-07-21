01b95f8fcdc07a02ced02859cff38e1d877664c3
sww1235 <sww1235@gmail.com>
2016-07-21 12:27:39 -0600

    stopping all remote repo checks for local only repos

--- gitCheckRemote.sh
+++ gitCheckRemote.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#TODO fix remote checking for local only repositories
 # Initialise counters:
 let count_all=0
 let count_changed=0
@@ -11,15 +10,19 @@ let verbose=0
 
 # Find git repos and loop over them:
 for repo in `find . -type d -name ".git"` # find returns a relative path with the .git directory at the
-do
-    let count_all=${count_all}+1
+  do
+  let count_all=${count_all}+1
 
-    # cd to the dir that contains .git/:
-    dir=`echo ${repo} | sed -e 's/\/.git/\//'` #replaces the /.git on the end of the returned path with /
-    cd ${dir}
+  # cd to the dir that contains .git/:
+  dir=`echo ${repo} | sed -e 's/\/.git/\//'` #replaces the /.git on the end of the returned path with /
+  cd ${dir}
 
-    # update remote refs
+  # update remote refs
+
+  if git ls-remote &> /dev/null
+  then
     git remote -v update &> /dev/null
+
 
     # command to check if pull is necessary
     pullNeeded=$(git rev-list HEAD..HEAD@{upstream} | wc -l | awk '{print $1}')
@@ -30,25 +33,23 @@ do
     #printf 'pullNeeded: %s pushNeeded: %s\n' ${pullNeeded} ${pushNeeded}
     # If there are changes, print some status and branch info of this repo:
     if [ ${pullNeeded} -ne "0" ] || [ ${pushNeeded} -ne "0" ]; then
-	    printf "\n%s needs attention\n" ${dir}
+      printf "\n%s needs attention\n" ${dir}
       printf "\n branch info\n\n"
-	    git branch -vvra
+      git branch -vvra
       printf "\n status info\n\n"
       git status
-	    let count_changed=${count_changed}+1
+      let count_changed=${count_changed}+1
       printf "%s\n" "--------------------------------------------------------------------------------"
     fi
+  else
+    if !git status &> /dev/null
+    then
+      git status
+    fi
+  fi
 
-
-    # # If verbose, print info in the case of no changes:
-    # if [ ${pullNeeded} -eq "0" ] && [ ${pushNeeded} -eq "0" ] && [ ${verbose} -ne 0 ]; then
-	  #   printf "Nothing to do for %s\n\n" ${dir}
-	  #   let count_unchanged=${count_unchanged}+1
-    #   printf "%s\n" "--------------------------------------------------------------------------------"
-    # fi
-
-    # cd back:
-    cd - &> /dev/null
+  # cd back:
+  cd - &> /dev/null
 done
 let count_unchanged=${count_all}-${count_changed}
 # Report status and exit:
